6483a462bea4ffa4c22f6373f0b72b68da4a2038
Caitlin Scarberry <caitlinscarberry@umail.ucsb.edu>
2018-01-19 23:11:45 -0800

    Player responses to input

--- gameplay/entity.py
+++ gameplay/entity.py
@@ -1,5 +1,31 @@
 class Entity:
     def __init__(self):
         pass
+    def processInputEvt():
+        pass
+    def update():
+        pass
 
-    
+class Player(Entity):
+    def __init__(self, x, y):
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.x = 0
+        self.y = 0
+        self.speed = 1
+
+    def processInputEvt(self):
+        gameEvts = []
+        if event.type == sdl2.SDL_KEYDOWN:
+            if event.key.keysym.sym == sdl2.SDLK_UP:
+                self.velocity_y = -self.speed
+            elif event.key.keysym.sym == sdl2.SDLK_DOWN:
+                self.velocity_y = self.speed
+            elif event.key.keysym.sym == sdl2.SDLK_RIGHT:
+                self.velocity_x = self.speed
+            elif event.key.keysym.sym == sdl2.SDLK_LEFT:
+                self.velocity_x = -self.speed
+        elif event.type == sdl2.SDL_KEYUP:
+            if event.key.keysym.sym in (sdl2.SDLK_UP, sdl2.SDLK_DOWN, sdl2.SDLK_RIGHT, sdl2.SDLK_LEFT):
+                self.velocity_x = 0
+                self.velocity_y = 0
