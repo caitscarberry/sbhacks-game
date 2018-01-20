27b151570e77e04a76dfa3bd81729ee04b99e520
Caitlin Scarberry <caitlinscarberry@umail.ucsb.edu>
2018-01-20 12:35:08 -0800

    Testing three players

--- caitlin_test.py
+++ caitlin_test.py
@@ -16,41 +16,65 @@ from gameplay.controls import ControlsState
 import platform
 import os
 
+WINDOW_SIZE = [1000, 650]
+SIDEBAR_WIDTH = 188
+GAME_WIDTH = 1000 - SIDEBAR_WIDTH
+
 def main():
+    if len(sys.argv) < 3:
+        print("Please provide player file name and player id")
+        sys.exit(1)
+
+    my_player_id = None
+    players_str = None
+    with open(sys.argv[1], "r") as player_file:
+        my_player_id = int(sys.argv[2])
+        players_str = player_file.read()
+
+    player_adds = players_str.split()
+    num_players = len(player_adds)
+    connections = [None] * num_players
+
+    for p in range(num_players):
+        if p == my_player_id:
+            continue
+        connection = None
+        iAmServer = p > my_player_id
+        if iAmServer:
+            connection = MessageQueueHolder(True)
+            connection.start_connect(player_adds[my_player_id], 25565 + p)
+        else:
+            connection = MessageQueueHolder(False)
+            connection.start_connect(player_adds[p], 25565 + my_player_id)
+
+        connections[p] = connection
+        print("Started connection to host " + str(p))
+
+    for p in range(num_players):
+        if p == my_player_id:
+            continue
+        connection = connections[p]
+        print("Connecting to host " + str(p) + " (" + connection.host + ":" + str(connection.port) + ")")
+        while not connection.connected:
+            SDL_Delay(100)
+
+        print("Connected to host " + str(p))
+        connection.start_update()
+
     window = init_window()
     running = True
     render = sdl2.ext.sprite.Renderer(window)
     my_render = graphics.render.Renderer(render)
 
-    connection = None
-    me = input()
-    iAmServer = me.startswith("s")
-    players = [Player(0, 0, 0), Player(1, 200, 200)]
-    my_player_id = None
-    if iAmServer:
-        my_player_id = 0
-        print("Server")
-        connection = MessageQueueHolder(True)
-        #changed this back for local testing, will not
-        #work across machines
-        connection.start_connect("localhost", 8080)
-    else:
-        my_player_id = 1
-        print("Client")
-        connection = MessageQueueHolder(False)
-        connection.start_connect("localhost", 8080)
-
-    print("Connecting")
-    while not connection.connected:
-        SDL_Delay(100)
-
-    print("Connected")
-    connection.start_update()
-
     col = Color(123, 123, 123)
 
     spritefac = graphics.sprite.SpriteFactory(render)
+
     playersprite = spritefac.from_file("./assets/players.png").subsprite(graphics.rect.Rect(0, 0, 100, 100))
+
+    players = []
+    for i in range(num_players):
+        players.append(Player(i, 50*i, 50))
 
     for player in players:
         player.load_sprite(spritefac)
@@ -72,11 +96,17 @@ def main():
                 if player_event.params["code"] == "NONE":
                     continue
                 game_events.append(player_event)
-                connection.send(player_event.serialize().encode("utf-8"))
+                for connection in connections:
+                    if connection is None:
+                        continue
+                    connection.send(player_event.serialize().encode("utf-8"))
 
-        while connection.queue.qsize() > 0:
-            msg = connection.queue.get()
-            game_events.append(GameEvent.deserialize(msg.decode("utf-8")))
+        for connection in connections:
+            if connection is None:
+                continue
+            while connection.queue.qsize() > 0:
+                msg = connection.queue.get()
+                game_events.append(GameEvent.deserialize(msg.decode("utf-8")))
 
         for event in game_events:
             if (event.params["type"] == "PLAYER"):
